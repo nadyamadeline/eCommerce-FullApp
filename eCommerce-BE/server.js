3fedc6410c3b9958440af94b03f56a03b227461c
nadyamadeline <nadyamadeline@gmail.com>
2021-04-04 21:18:12 +0700

    create mongoDB product api

--- eCommerce-BE/server.js
+++ eCommerce-BE/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js"; // in server-side programming, we need to append the .js extension
-import UserRouter from "./routers/user.js";
+import UserRouter from "./routers/user.js"; // in server-side programming, we need to append the .js extension
+import ProductRouter from "./routers/product.js";
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/canopyco", {
@@ -10,22 +10,11 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/canopyco", {
   useCreateIndex: true,
 });
 
-// display product detail
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === Number(req.params.id));
-  // need to convert req.params.id to number as it returns a string while our id is denoted as number
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-// display product data
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
 // show sample user
 app.use("/api/users", UserRouter);
+
+// show sample products
+app.use("/api/products", ProductRouter);
 
 // set the root of our server
 app.get("/", (req, res) => {
@@ -36,6 +25,7 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
+
 const port = process.env.PORT || 5000;
 // listening on port 5000
 app.listen(port, () => {
